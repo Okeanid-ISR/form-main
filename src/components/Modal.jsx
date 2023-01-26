400678c57a482e2af7b4824d47396520bfab621e
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-01-26 22:16:28 +0200

    feat(modal): added

---
+++ src/components/Modal.jsx
@@ -0,0 +1,20 @@
+const Modal = (props) => {
+  return (
+    <div className='modal fade' tabIndex={-1} {...props}>
+      <div className='modal-dialog modal-dialog-centered'>
+        <div className='modal-content'>
+          <div className='position-absolute top-0 end-0 p-2'>
+            <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'/>
+          </div>
+          <div className='modal-header justify-content-center'>
+            <p className='modal-title h1' id='exampleModalLabel'>
+              Welcome back.
+            </p>
+          </div>
+          <div className='modal-body'>{props.children}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Modal
