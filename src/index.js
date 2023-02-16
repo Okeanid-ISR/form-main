02b5d592f15f557baf3ee7707b1d17353b204ecc
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-02-16 21:40:03 +0200

    feat(app): added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { createRoot } from "react-dom/client"
-import { App } from "./App"
-import { Modal } from 'bootstrap'
-import './styles.scss'
+  import { createRoot } from "react-dom/client"
+  import { App } from "./App"
+  import { Modal } from 'bootstrap'
+  import './styles.scss'
 
-const container = document.getElementById("app")
-const root = createRoot(container)
-root.render(<App />)
+  const container = document.getElementById("app")
+  const root = createRoot(container)
+  root.render(<App />)
